8c297d860ae9cbd6b8081188d626d53b2f60ee76
mr_noppo <takizawa20383461@gmail.com>
2025-02-22 18:31:43 +0900

    visx(グラフ系)

---
+++ src/components/visx/Drag/index.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Component } from "./index";
+
+const meta = {
+  component: Component,
+} satisfies Meta<typeof Component>;
+
+export default meta;
+type Story = StoryObj<typeof Component>;
+
+export const Nomarl: Story = {
+  render: () => {
+    return <Component width={100000} height={100000} />;
+  },
+};
